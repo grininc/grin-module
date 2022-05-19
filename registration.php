b5c24dd4a7cadcc0bc051d299fca85595b2eac48
Sebastian Colandrea <scolandrea@gmail.com>
2022-05-19 19:07:36 -0300

    Fix the issue with webapi tests autoload during di compile process

--- registration.php
+++ registration.php
@@ -7,8 +7,4 @@ declare(strict_types=1);
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(
-    ComponentRegistrar::MODULE,
-    'Grin_Module',
-    isset($file) && realpath($file) == __FILE__ ? dirname($file) : __DIR__
-);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Grin_Module', __DIR__);
