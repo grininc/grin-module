cc4a059f7788e80d72ff944e4cd4dd9c1d23e995
mmmelo <marcel.melo@grin.co>
2023-06-26 09:48:26 -0400

    GN-17876: GRIN Magento app does not trim historical Webhook Queue Messages

--- Model/Queue/ResourceModel/ResponseHandler.php
+++ Model/Queue/ResourceModel/ResponseHandler.php
@@ -31,5 +31,46 @@ class ResponseHandler
             $connection->getTableName('grin_queue_message_status'),
             $data
         );
+
+        $this->deleteQueueMessagesSucceed();
+        $this->deleteQueueMessagesErrors();
+    }
+
+    private function deleteQueueMessagesSucceed()
+    {
+        $connection = $this->resourceConnection->getConnection();
+        $queueMessageStatusTable = $connection->getTableName('queue_message_status');
+        $queueMessageTable = $connection->getTableName('queue_message');
+
+        $select = $connection->select()
+            ->from($queueMessageStatusTable, 'message_id')
+            ->where('updated_at <= ?', date('Y-m-d', strtotime('-30 days')))
+            ->where('queue_id = 5')
+            ->where('status = 4');
+
+        $queueIds = $connection->fetchCol($select);
+
+        if (!empty($queueIds)) {
+            $connection->delete($queueMessageTable, ['id IN (?)' => $queueIds]);
+        }
+    }
+
+    private function deleteQueueMessagesErrors()
+    {
+        $connection = $this->resourceConnection->getConnection();
+        $queueMessageStatusTable = $connection->getTableName('queue_message_status');
+        $queueMessageTable = $connection->getTableName('queue_message');
+
+        $select = $connection->select()
+            ->from($queueMessageStatusTable, 'message_id')
+            ->where('updated_at <= ?', date('Y-m-d', strtotime('-6 months')))
+            ->where('queue_id = 5')
+            ->where('status <> 4');
+
+        $queueIds = $connection->fetchCol($select);
+
+        if (!empty($queueIds)) {
+            $connection->delete($queueMessageTable, ['id IN (?)' => $queueIds]);
+        }
     }
 }
